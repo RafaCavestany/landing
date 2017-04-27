c7963ff6aee50c92aea2c9daa4ada035e27e4e03
Michael Diego <michhdiego@gmail.com>
2017-04-26 23:33:13 -0300

    Finish menu navigation

--- src/js/hamburger-menu.js
+++ src/js/hamburger-menu.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   $('.js-hamburger-menu').click(function() {
+    // Clicking on hamburger switches its icon
     $(this).toggleClass('active');
+    // set header as active
+    $(this).closest('.js-hamburger-container').toggleClass('active');
+    // set menu as active
     $('.js-menu-overlay').toggleClass('active');
+  });
+  $('.js-menu-item').click(function() {
+    // Click on an anchor, remove menu overlay
+    $('.js-hamburger-menu').removeClass('active');
+    $('.js-menu-overlay').removeClass('active');
+    $('.js-hamburger-container').removeClass('active');
   });
 });
