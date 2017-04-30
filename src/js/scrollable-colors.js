08a0da1b6772a988b08f5bd79c737d508b0c10d8
Michael Diego <michhdiego@gmail.com>
2017-04-30 07:16:12 -0300

    Use const instead of var

--- src/js/scrollable-colors.js
+++ src/js/scrollable-colors.js
@@ -1,23 +1,23 @@
 // This code is based on this pen: https://codepen.io/daveredfern/pen/zBGBJV
 $(document).ready(function() {
-  var $body = $('body');
+  const $body = $('body');
   $body.addClass('color-' + $('.js-panel').data('color'));
 });
 
 $(window).scroll(function() {
-  var $window = $(window),
+  const $window = $(window),
     $body = $('body'),
     $panels = $('.js-panel');
 
   // Change 33% earlier than scroll position so colour is there when you arrive.
-  var scroll = $window.scrollTop() + ($window.height() / 3);
+  const scroll = $window.scrollTop() + ($window.height() / 3);
 
   $panels.each(function() {
-    var $this = $(this);
+    const $this = $(this);
 
     // if position is within range of this panel.
     // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-    // Remember we set the scroll to 33% earlier in scroll var.
+    // Remember we set the scroll to 33% earlier in scroll const.
     if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
 
       // Remove all classes on body with color-
