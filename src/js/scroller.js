be980589cadc885dc6c82254fc1b514bd2aade79
Michael Diego <michhdiego@gmail.com>
2017-05-03 04:07:42 -0300

    Make arrow down for scrollable sections work

--- src/js/scroller.js
+++ src/js/scroller.js
@@ -3,10 +3,19 @@
 // Define funcs
 const getDownArrowChild = function($ancestor) {
   return $ancestor.getElementsByClassName('js-scroll-to-content')[0];
-}
+};
 
-const scrollToContentHandler = function($scrollableSection, index) {
-  $scrollableSection.classList.add('hidden');
+const getElementInfo = function($element) {
+  return $element.getBoundingClientRect();
+};
+
+const scrollToContentHandler = function($section, index) {
+  if (index === 0) {
+    // if its the first page, we only need to scroll half of the height
+    // of the container
+    const sectionHeight = getElementInfo($section).height;
+    $section.style.top = `${sectionHeight * -1}px`;
+  }
 };
 
 // Trigger docReady
@@ -19,10 +28,12 @@ window.onload = function() {
       $downArrow = getDownArrowChild($element);
       // TODO: nav: hide_top_arrow
       // TODO: nav: set_current_section_name
+      // TODO: set base color?
     }
     // If downArrow is defined, add event listener
     if ($downArrow) {
-      $downArrow.addEventListener('click', function() {
+      $downArrow.addEventListener('click', function(e) {
+        e.preventDefault();
         scrollToContentHandler($element, index);
       });
     }
