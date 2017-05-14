d2899d6bd4af2fa7322ad78bdf6af2f81ece3f2b
Michael Diego <michhdiego@gmail.com>
2017-05-14 06:59:38 -0300

    Link Start a project with its section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import { animateScroll } from 'react-scroll';
+
 class Header extends Component {
+  handleClick(ev) {
+    ev.preventDefault();
+    animateScroll.scrollToBottom();
+  };
+
   render() {
     return (
       <header className="header header--desktop">
-        <a href="#project" className="header__link">
+        <a href="#" className="header__link" onClick={this.handleClick}>
           <i className="icon icon-party-popper header__link__icon"></i>
           <span className="header__link__text">
             Start a Project
