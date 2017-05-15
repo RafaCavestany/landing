c387265c28287a5c9121f14a2d5f2975fb603550
Michael Diego <michhdiego@gmail.com>
2017-05-15 05:22:38 -0300

    Add mobile header + menu

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -1,6 +1,68 @@
 import React, { Component } from 'react';
 
 class MobileHeader extends Component {
+  constructor (props) {
+    super(props);
+    this.scrollTollerance = 100;
+    this._initState = {
+      isMenuActive: false
+    };
+    this.state = this._initState;
+    this.handleClose = this.handleClose.bind(this);
+    this.handleNavClick = this.handleNavClick.bind(this);
+    this.handleMenuClick = this.handleMenuClick.bind(this);
+  };
+
+  handleNavClick(ev) {
+    this.handleClose();
+  };
+
+  handleClose() {
+    this.setState(this._initState);
+  };
+
+  handleMenuClick() {
+    this.setState({
+      isMenuActive: !this.state.isMenuActive
+    });
+  };
+
+  getActiveClass() {
+    const {isMenuActive} = this.state;
+    return isMenuActive ? 'active' : '';
+  };
+
+  renderMenuItems() {
+    const sections = [
+      { name: 'intro'},
+      { name: 'about'},
+      { name: 'work'},
+      { name: 'project'}
+    ];
+    return sections.map((section, index) => {
+      return (
+        <li key={`${section.name}-${index}`}
+          className="menu__item"
+          onClick={this.handleNavClick}
+        >
+          <a href={`#${section.name}`} className="u-txt-capitalize">
+            {section.name}
+          </a>
+        </li>
+      );
+    });
+  }
+
+  renderMenu(zIndex) {
+    return (
+      <div className={`menu ${this.getActiveClass()}`}>
+        <ul className="menu__list">
+          {this.renderMenuItems()}
+        </ul>
+      </div>
+    );
+  };
+
   render() {
     return (
       <header className="header header--mobile">
@@ -9,11 +71,12 @@ class MobileHeader extends Component {
             Rafa Cavestany
           </span>
         </a>
-        <div className="hamburger">
+        <div className={`hamburger ${this.getActiveClass()}`} onClick={this.handleMenuClick}>
           <div className="hamburger__container">
             <span className="hamburger__item"></span>
           </div>
         </div>
+        {this.renderMenu()}
       </header>
     );
   }
