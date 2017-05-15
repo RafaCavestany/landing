b5c80c5a9cdb3055f62eb016de76e437511cbb39
Michael Diego <michhdiego@gmail.com>
2017-05-15 06:24:06 -0300

    Fix intro for good

--- src/components/sections/Intro.js
+++ src/components/sections/Intro.js
@@ -6,15 +6,11 @@ class Intro extends Component {
       <article id="rafael" className="card banner">
         <h1 className="card__title banner__title">
           <div className="banner__container">
-            Rafa Cavestany is an Award-winning&nbsp;
-            <span className="u-inline-block u-txt-underlined">
+            Rafa Cavestany is an Award-winning <span className="u-inline-block u-txt-underlined">
               Designer
-            </span>
-            &nbsp;&amp;&nbsp;
-            <span className="u-inline-block u-txt-underlined">
+            </span> &amp; <span className="u-inline-block u-txt-underlined">
               Creative Director
-            </span>
-            &nbsp;working in digital projects for all over the world.
+            </span> working in digital projects for all over the world.
           </div>
         </h1>
       </article>
